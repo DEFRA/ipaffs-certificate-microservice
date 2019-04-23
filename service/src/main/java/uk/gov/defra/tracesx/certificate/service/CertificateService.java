68d38930b70d7521fb2f082ec9e7e7fa8f438240
Sean Treanor <seant@kainos.com>
2019-04-23 09:46:29 +0100

    Applying checkstyle changes to conform with checkstyles CLI report

--- service/src/main/java/uk/gov/defra/tracesx/certificate/service/CertificateService.java
+++ service/src/main/java/uk/gov/defra/tracesx/certificate/service/CertificateService.java
@@ -3,8 +3,8 @@ package uk.gov.defra.tracesx.certificate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.defra.tracesx.certificate.model.Certificate;
-import uk.gov.defra.tracesx.certificate.utils.ReferenceNumberGenerator;
 import uk.gov.defra.tracesx.certificate.utils.CertificatePdfGenerator;
+import uk.gov.defra.tracesx.certificate.utils.ReferenceNumberGenerator;
 import uk.gov.defra.tracesx.certificate.utils.UriParser;
 
 import java.net.URI;
@@ -22,7 +22,8 @@ public class CertificateService {
     this.uriParser = uriParser;
   }
 
-  public Certificate getPdf(ReferenceNumberGenerator reference, Supplier<String> htmlProvider, URI uri) {
+  public Certificate getPdf(ReferenceNumberGenerator reference, Supplier<String> htmlProvider,
+      URI uri) {
     String content = htmlProvider.get();
     URI baseUri = uriParser.getBaseUri(uri);
     return new Certificate(reference, pdfGenerator.createPdf(content, baseUri));
