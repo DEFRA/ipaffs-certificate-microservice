7ae52b3e29f223f596a18713b5b08db69a76c01a
ryan.beattie <ryan.beattie@defra.onmicrosoft.com>
2023-08-16 14:16:27 +0100

    IMTA-13294 remove password from logs

--- service/src/main/java/uk/gov/defra/tracesx/certificate/CertificateApplication.java
+++ service/src/main/java/uk/gov/defra/tracesx/certificate/CertificateApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {
     DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class})
+    HibernateJpaAutoConfiguration.class,
+    UserDetailsServiceAutoConfiguration.class})
 @ComponentScan({"uk.gov.defra.tracesx"})
 public class CertificateApplication {
   public static void main(String[] args) {
